492134cc716f0ea9476ef9b3b5e6475f52f07e39
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-08-18 14:28:45 -0500

    Initial commit - added supplimentary applications, overall file structure, gitignore, readme and license

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CompSec-Experiments
+# CompSec Experiements
+This is a student project to learn more about x86 executables structure,
+play with injections into executable files etc.
+
+
+# License
+MIT. Do whatever you want with source code, except of illegal things.
---
+++ Supplimentary/Host/Main.cpp
@@ -0,0 +1,11 @@
+/****************************************************************
+ *
+ *	Empty application that does nothing. 
+ *
+ *	Compiled executable is needed to host other app code.
+ *
+ ****************************************************************/
+
+int main() {
+	return 0;
+}
