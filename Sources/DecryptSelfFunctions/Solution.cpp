7b563879e24552c4af6d143954f582c119a7dc06
Mikhail Mordvinkin <mordvinkinm@gmail.com>
2018-08-26 19:03:32 -0500

    Added DecryptSelfFunctions code

---
+++ Sources/DecryptSelfFunctions/Solution.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Injector.h"
+#include "Io.h"
+#include "FunctionEncryptor.h"
+
+const std::string Key = "THIS IS SECRED KEY!";
+
+void method_01(int cnt){
+    printf("Hello, I am method 01. I receive integer as parameter, but absolutely useless :(");
+}
+
+int method_02(std::string data) {
+    printf("Hello, I am method_02. I simply calculates string lenth");
+    int result = data.size();
+
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 1) {
+        printf("Cannot get app path, something went wrong");
+        return EXIT_FAILURE;
+    }
+
+    decrypt(&method_01, Key);
+    decrypt(&method_02, Key);
+    method_01(method_02("abcdefgh"));
+    encrypt(&method_01, Key);
+    encrypt(&method_02, Key);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
---
+++ Supplimentary/DecryptSelfFunctions_PostbuildEncryptor/Solution.cpp
@@ -0,0 +1,37 @@
+#include "FunctionEncryptor.h"
+#include "Io.h"
+
+// list of function offsets
+size_t offsetCnt = 2;
+int offsets[2] = { 
+    0x9b0   // method_01
+    ,0x9d0  // method_02
+};
+
+const std::string Key = "THIS IS SECRED KEY!";
+
+int main(int argc, char* argv[]) {
+    std::string filename;
+    std::cout<<"Enter path to file"<<std::endl;
+    getline(std::cin, filename);
+
+    byte_array file = read_file(filename);
+
+    for (size_t i = 0; i < offsetCnt; i++){
+        char* pointer = file.pointer + offsets[i];
+
+        for (size_t i = 0, key_index = 0; i < file.size; i++, key_index = (key_index + 1) % Key.size()){
+            if (*(pointer + i) == PreRetVal && *(pointer + i + 1) == RetVal)
+                break;
+
+            *(pointer + i) = *(pointer + i) ^ Key[key_index];
+        }
+    }
+
+    write_file(filename, file);
+
+    printf("Encryption completed\n");
+
+    system("pause");
+    return 0;
+}
